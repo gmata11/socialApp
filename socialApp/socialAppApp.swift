68174a09fca3ffdd59308224d36248a69006559f
gmata11 <gerard@dekalabs.com>
2020-09-25 16:01:29 +0200

    First commit: Creating project

---
+++ socialApp/socialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  socialAppApp.swift
+//  socialApp
+//
+//  Created by Gerard Mata Carrera on 25/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct socialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
