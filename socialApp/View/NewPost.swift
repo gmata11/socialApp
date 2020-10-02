c2aac8118f5aa08e7e25be3658f5708295b7c5a9
gmata11 <gerard@dekalabs.com>
2020-10-02 12:02:49 +0200

    added new post

---
+++ socialApp/View/NewPost.swift
@@ -0,0 +1,20 @@
+//
+//  NewPost.swift
+//  socialApp
+//
+//  Created by Gerard Mata Carrera on 29/09/2020.
+//
+
+import SwiftUI
+
+struct NewPost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewPost_Previews: PreviewProvider {
+    static var previews: some View {
+        NewPost()
+    }
+}
---
+++ socialApp/ViewModel/NewPostModel.swift
@@ -0,0 +1,20 @@
+//
+//  NewPostModel.swift
+//  socialApp
+//
+//  Created by Gerard Mata Carrera on 29/09/2020.
+//
+
+import SwiftUI
+
+struct NewPostModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewPostModel_Previews: PreviewProvider {
+    static var previews: some View {
+        NewPostModel()
+    }
+}
